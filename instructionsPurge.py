7f7b296452e6ae41dbcbc5e3091f40fe6e88d8aa
Alexander Scott <xander@cadcoder.com>
2015-08-27 10:48:26 +1000

    How to Recover Purge

---
+++ instructionsPurge.py
@@ -0,0 +1,19 @@
+import os
+
+baseDir = "/Volumes/"
+
+purgeList = os.curdir + "/purgeList.txt"
+
+if os.path.exists(purgeList):
+    lines = []
+    # Read the files to be purged
+    with open(purgeList) as f:
+        lines = f.readlines()
+        lines = [line.rstrip('\n') for line in open(purgeList)]
+
+    # Delete each file
+    for l in lines:
+        fileToPurge = baseDir + l[7:]
+        if os.path.exists(fileToPurge):
+            print "Removing File: ", fileToPurge
+            os.remove(fileToPurge)
